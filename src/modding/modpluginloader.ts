1b726eff3da1e8ef31aba65dd134ae0384985863
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-09-04 17:04:23 +0200

    fix: add missing modpluginloader validator

--- src/modding/modpluginloader.ts
+++ src/modding/modpluginloader.ts
@@ -1,4 +1,17 @@
-import type { DeprecatedModLoader, ModLoader } from './modloaders';
-import type { PluginLoader } from './pluginloaders';
+import { z } from 'zod';
+
+import {
+	deprecatedModLoaders,
+	modLoaders,
+	type DeprecatedModLoader,
+	type ModLoader,
+} from './modloaders';
+import { pluginLoaders, type PluginLoader } from './pluginloaders';
+
+export const modPluginLoaderValidator = z.enum([
+	...modLoaders,
+	...deprecatedModLoaders,
+	...pluginLoaders,
+]);
 
 export type ModPluginLoader = ModLoader | DeprecatedModLoader | PluginLoader;
