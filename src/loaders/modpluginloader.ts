5a1db2d36344d422d0430ca7774212a6bb1f5b4a
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-11-03 00:34:02 +0100

    fix

--- src/loaders/modpluginloader.ts
+++ src/loaders/modpluginloader.ts
@@ -1,11 +1,17 @@
 import z from 'zod';
 
-import { deprecatedModLoaders, modLoaders } from './modloaders';
-import { pluginLoaders } from './pluginloaders';
+import {
+	deprecatedModLoaders,
+	modLoaders,
+	type DeprecatedModLoader,
+	type ModLoader,
+} from './modloaders';
+import { pluginLoaders, type PluginLoader } from './pluginloaders';
 
-/* @ts-expect-error */
-export const modPluginLoaders = modLoaders.concat(deprecatedModLoaders).concat(pluginLoaders);
+export type ModPluginLoader = ModLoader | DeprecatedModLoader | PluginLoader;
+
+export const modPluginLoaders: ModPluginLoader[] = [
+	...new Set([...modLoaders, ...deprecatedModLoaders, ...pluginLoaders]),
+];
 
 export const modPluginLoaderValidator = z.enum(modPluginLoaders);
-
-export type ModPluginLoader = (typeof modPluginLoaders)[number];
