391a7cc6ad45f6fb7f38c323e41d64aa4a3296ac
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-11-09 17:38:49 +0100

    feat: geyser proxy server

--- src/loaders/proxyservers/index.ts
+++ src/loaders/proxyservers/index.ts
@@ -2,6 +2,7 @@ import z from "zod";
 
 export const proxyServers = [
 	"bungeecord",
+	"geyser",
 	"velocity",
 	"waterfall",
 ] as const satisfies string[];
@@ -13,5 +14,6 @@ export type ProxyServer = (typeof proxyServers)[number];
 /*  */
 
 export { bindings as bungeecordBindings } from "./bungeecord";
+export { bindings as geyserBindings } from "./geyser";
 export { bindings as velocityBindings } from "./velocity";
 export { bindings as waterfallBindings } from "./waterfall";
