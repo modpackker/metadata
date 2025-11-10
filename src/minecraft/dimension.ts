b527055d9faa306c9dd9718e3e88beb22c99c063
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-11-10 23:33:54 +0100

    fix: naming

--- src/minecraft/dimension.ts
+++ src/minecraft/dimension.ts
@@ -2,6 +2,6 @@ import z from 'zod';
 
 export const dimensions = ['overworld', 'nether', 'end'] as const satisfies string[];
 
-export const dimensionSchema = z.enum(dimensions);
+export const dimensionValidator = z.enum(dimensions);
 
 export type Dimension = (typeof dimensions)[number];
