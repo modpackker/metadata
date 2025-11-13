d4fde47abb310f4a6ecbafebc100b51c597abfc5
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-11-13 17:45:59 +0100

    feat: loader type

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,17 @@
 import z from 'zod';
 
+import type { DeprecatedModLoader, ModLoader } from './modloaders';
+import type { ShaderLoader } from './shaderloaders';
+import type { PluginLoader } from './pluginloaders';
+import type { ProxyServer } from './proxyservers';
+
 export const loaderTypes = ['modloader', 'pluginloader', 'proxyservers', 'shaderloader'];
 
 export type LoaderType = (typeof loaderTypes)[number];
 
 export const loaderTypeValidator = z.enum(loaderTypes);
+
+export type Loader = ModLoader | DeprecatedModLoader | ShaderLoader | PluginLoader | ProxyServer;
 
 /*  */
 
