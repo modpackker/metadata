22949cf5729d602db3f0a83326a840440821f989
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-03-20 10:48:47 +0100

    fix: type mismatch

--- src/loaders/modpluginloader.ts
+++ src/loaders/modpluginloader.ts
@@ -10,8 +10,8 @@ import { pluginLoaders, type PluginLoader } from './pluginloaders';
 
 export type ModPluginLoader = ModLoader | DeprecatedModLoader | PluginLoader;
 
-export const modPluginLoaders: ModPluginLoader[] = [
+export const modPluginLoaders = [
 	...new Set([...modLoaders, ...deprecatedModLoaders, ...pluginLoaders]),
-];
+] as const satisfies ModPluginLoader[];
 
 export const modPluginLoaderValidator = z.enum(modPluginLoaders);
