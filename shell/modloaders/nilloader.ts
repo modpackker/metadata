16a4e8a1a91a3558b7900ff9af64e60d614f00e8
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-04-01 14:14:56 +0200

    chore: refactor

--- shell/modloaders/nilloader.ts
+++ shell/modloaders/nilloader.ts
@@ -3,17 +3,16 @@ import { mcVersions, type McVersion } from '../../src/minecraft/version/_index';
 import { writeLoaderSync } from '../lib';
 
 export const nilloader = async () => {
-	const loaderVersion = [
-		...(
-			await (
-				await fetch('https://repo.sleeping.town/com/unascribed/nilloader/maven-metadata.xml')
-			).text()
-		)
-			.matchAll(/<version>(.+)<\/version>/g)
-			.map((match) => {
-				return match[1];
-			}),
-	].at(-1) as McVersion;
+	const loaderVersion = (
+		await (
+			await fetch('https://repo.sleeping.town/com/unascribed/nilloader/maven-metadata.xml')
+		).text()
+	)
+		.matchAll(/<version>(.+)<\/version>/g)
+		.map((match) => {
+			return match[1];
+		})
+		.at(-1) as McVersion;
 
 	const bindings: Bindings = Object.fromEntries(
 		mcVersions.map((mcVersion) => {
