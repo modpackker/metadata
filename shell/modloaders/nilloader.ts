d54ccf34c8821fd3e113751c2a1011968dd19e80
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-04-09 21:41:45 +0200

    fix: type

--- shell/modloaders/nilloader.ts
+++ shell/modloaders/nilloader.ts
@@ -3,12 +3,13 @@ import { mcVersions, type McVersion } from '../../src/minecraft/version/_index';
 import { writeLoaderSync } from '../lib';
 
 export const nilloader = async () => {
-	const loaderVersion = (
-		await (
-			await fetch('https://repo.sleeping.town/com/unascribed/nilloader/maven-metadata.xml')
-		).text()
-	)
-		.matchAll(/<version>(.+)<\/version>/g)
+	const loaderVersion = [
+		...(
+			await (
+				await fetch('https://repo.sleeping.town/com/unascribed/nilloader/maven-metadata.xml')
+			).text()
+		).matchAll(/<version>(.+)<\/version>/g),
+	]
 		.map((match) => {
 			return match[1];
 		})
